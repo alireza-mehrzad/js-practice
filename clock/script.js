4dfc502b3b5776fd73d71b3be94efd7fcf2e1a2a
alireza-mehrzad <alireza.mehrzad369@gmail.com>
2023-03-26 04:10:02 -0700

    script drawNumbers add

--- clock/script.js
+++ clock/script.js
@@ -32,3 +32,25 @@ function drawFace(ctx, radius)
     ctx.fillStyle = "#333";
     ctx.fill();
 }
+
+
+
+
+function drawNumbers(ctx, radius)
+{
+    let ang;
+    ctx.font = radius * 0.15 + "px arial";
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    for (let num = 1; num < 13; num++)
+    {
+        ang = (num * Math.PI) / 6;
+        ctx.rotate(ang);
+        ctx.translate(0, -radius * 0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, radius * 0.85);
+        ctx.rotate(-ang);
+    }
+}
