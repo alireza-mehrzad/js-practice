e7d9d3efe33dfa846ba3f38bfdd0a83c6fede2b4
alireza-mehrzad <alireza.mehrzad369@gmail.com>
2023-03-27 05:10:45 -0700

    time and update func

--- count-down/script.js
+++ count-down/script.js
@@ -7,3 +7,24 @@ function getTimeRemaining(endTime)
     const days = Math.floor(total / (1000 * 60 * 60 * 24));
     return { total, days, hours, minutes, seconds };
 }
+
+
+function initializeClock(endTime)
+{
+    const clock = document.querySelector("#clock");
+    const daysSpan = clock.querySelector("#days");
+    const hoursSpan = clock.querySelector("#hours");
+    const minutesSpan = clock.querySelector("#minutes");
+    const secondsSpan = clock.querySelector("#seconds");
+
+    function updateClock()
+    {
+        const { total, days, hours, minutes, seconds } = getTimeRemaining(endTime);
+
+        daysSpan.innerText = days;
+        hoursSpan.innerText = ("0" + hours).slice(-2);
+        minutesSpan.innerText = ("0" + minutes).slice(-2);
+        secondsSpan.innerText = ("0" + seconds).slice(-2);
+
+        total <= 0 && clearInterval(timeInterval);
+    }
