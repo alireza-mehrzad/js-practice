e6930a0ef18c42e03a4d4b89671b46de0738f46a
alireza-mehrzad <alireza.mehrzad369@gmail.com>
2023-03-27 05:10:03 -0700

    getTimeRemaining func

---
+++ count-down/script.js
@@ -0,0 +1,9 @@
+function getTimeRemaining(endTime)
+{
+    const total = Date.parse(endTime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+    return { total, days, hours, minutes, seconds };
+}
